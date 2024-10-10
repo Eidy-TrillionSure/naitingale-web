eeea45e4aa554711d780cb46aa5978b2c0e52af8
Benjapol Worakan <benwrk@live.com>
2024-10-11 03:42:46 +0700

    configure colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,14 @@
 export default {
   content: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
   theme: {
-    extend: {}
+    extend: {},
+    colors: {
+      primary: '#4a5c78',
+      secondary: '#b3bcc6',
+      accent: '#449bfc',
+      background: '#f5f7fa',
+      headings: '#1c2b49'
+    }
   },
   plugins: []
 }
