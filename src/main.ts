95dc7c6c395efcfe4c01d4b2846cfc37a5ecd756
Benjapol Worakan <benwrk@live.com>
2024-10-21 14:50:51 +0700

    add fontawesome icons

--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,17 @@
 import './assets/main.css'
 
-import { createApp } from 'vue'
 import { createPinia } from 'pinia'
+import { createApp } from 'vue'
 
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { fab } from '@fortawesome/free-brands-svg-icons'
+import { far } from '@fortawesome/free-regular-svg-icons'
+import { fas } from '@fortawesome/free-solid-svg-icons'
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+import Aura from '@primevue/themes/aura'
+import PrimeVue from 'primevue/config'
 import App from './App.vue'
 import router from './router'
-import PrimeVue from 'primevue/config'
-import Aura from '@primevue/themes/aura'
-
 const app = createApp(App)
 
 app.use(createPinia())
@@ -21,9 +25,20 @@ app.use(PrimeVue, {
         name: 'primevue',
         order: 'tailwind-base, primevue, tailwind-utilities'
       },
+      zIndex: {
+        modal: 1100, //dialog, drawer
+        overlay: 1000, //select, popover
+        menu: 1000, //overlay menus
+        tooltip: 1100 //tooltip
+      },
       darkModeSelector: '.dark'
     }
   }
 })
+
+library.add(fas)
+library.add(fab)
+library.add(far)
+app.component('font-awesome-icon', FontAwesomeIcon)
 
 app.mount('#app')
