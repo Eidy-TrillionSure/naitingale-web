8277a0159b5e188adf2bc694f2448919d88d7546
Benjapol Worakan <benwrk@live.com>
2024-10-22 04:13:35 +0700

    update banner

---
+++ src/components/FeatureBanner.vue
@@ -0,0 +1,14 @@
+<template>
+  <section
+    class="bg-[url('@/assets/banner-1.png')] bg-cover bg-center aspect-[3] flex flex-col justify-between text-background p-8"
+  >
+    <div class="flex flex-col ml-auto w-1/4 drop-shadow-md">
+      <h1>Simulated Patient Digital Twin</h1>
+      <div>ช่วยให้นักศึกษาแพทย์/แพทย์ ได้เรียนรู้แบบ Interactive ในการซักประวัติคนไข้</div>
+    </div>
+    <div class="flex flex-col w-1/4 drop-shadow-md">
+      <h1>Pre-screening Digital Twin</h1>
+      <div>เพื่อช่วยคัดกรองผู้ป่วย และทำให้พบแพทย์ได้ถูกต้อง ลดการใช้พยาบาลในการคัดกรองผู้ป่วย</div>
+    </div>
+  </section>
+</template>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -3,12 +3,14 @@ import AboutUs from '@/components/AboutUs.vue'
 import ContactUs from '@/components/ContactUs.vue'
 import HowToUse from '@/components/HowToUse.vue'
 import HeroElement from '@/components/HeroElement.vue'
+import FeatureBanner from '@/components/FeatureBanner.vue'
 </script>
 
 <template>
   <main class="container flex flex-col gap-y-4 justify-center">
     <HeroElement></HeroElement>
     <HowToUse></HowToUse>
+    <FeatureBanner></FeatureBanner>
     <AboutUs></AboutUs>
     <ContactUs></ContactUs>
   </main>
